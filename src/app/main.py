598c7d0082728998188aeab8e3db4ad963b7a930
Anton Novojilov <wowlass63@gmail.com>
2026-03-21 23:39:12 +0400

    health and test

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
+from src.app.api.routers.health import router as health_router
 
-app = FastAPI()
+def create_app() -> FastAPI:
+    """Создает приложение"""
+    app = FastAPI(
+        title="Сервис для регистрации и авторизации пользователей",
+        description="Сервис на FastAPI для регистрации и авторизации пользователей",
+        version="1.0.0",
+    )
+    app.include_router(health_router)
+    return app
+
+app = create_app()
+
+
 
