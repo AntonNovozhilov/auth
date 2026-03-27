e1fca75fbc7135a3bf032fa1cc73fcf8dc380218
Anton Novojilov <wowlass63@gmail.com>
2026-03-27 15:49:08 +0400

    register_schema -> pydantic v2 style

--- src/app/schemas/register_schema.py
+++ src/app/schemas/register_schema.py
@@ -1,18 +1,36 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr, SecretStr, field_validator, ConfigDict
+from datetime import date
+from pydantic.types import StringConstraints
+from typing import Annotated
+
+NamePattern = Annotated[str, StringConstraints(min_length=2, max_length=50)]
+NickNamePattern = Annotated[NamePattern, StringConstraints(min_length=4, max_length=50)]
 
 
 class RegisterRequest(BaseModel):
     """Схема для регистрации пользователя"""
 
-    first_name: str = Field(example="John", description="Имя")
-    last_name: str = Field(example="Doe", description="Фамилия")
-    email: str = Field(
-        example="john.doe@example.com",
+    first_name: NamePattern = Field(json_schema_extra={"example": "John"}, description="Имя")
+    last_name: NamePattern = Field(json_schema_extra={"example": "Doe"}, description="Фамилия")
+    email: EmailStr = Field(
+        json_schema_extra={"example": "john.doe@example.com"},
         description="Email",
     )
-    nickname: str = Field(example="johndoe", description="Никнейм")
-    birth_date: str = Field(
-        example="2000-01-01",
+    nickname: NickNamePattern = Field(
+        json_schema_extra={"example": "johndoe"}, description="Никнейм"
+    )
+    birth_date: date = Field(
+        json_schema_extra={"example": "2000-01-01"},
         description="Дата рождения",
     )
-    password: str = Field(example="Password", description="Пароль")
+    password: SecretStr = Field(json_schema_extra={"example": "Password"}, description="Пароль")
+
+    model_config = ConfigDict(
+        extra="forbid",
+    )
+
+    @field_validator("password", mode="after")
+    def validate_password(cls, v: SecretStr) -> SecretStr:
+        if len(v) < 8:
+            raise ValueError("Пароль не должен быть короче 8 символов")
+        return v
